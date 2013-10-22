5ad0163de7638b5926d34b3805512731dbf653a8
ericdum <formateric@gmail.com>
2013-10-22 15:40:27 +0800

    bug

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 <title>木匠手记</title>
 
 <?php $url = "https://github.com/ericdum/mujiang.info/issues/1"?>
-<div>木匠手记正在制作中，请先访问Github Blog: <a href="<?=$url?>"><?=$url?></a>
+<div>木匠手记正在制作中，请先访问Github Blog: <a href="<?php echo $url?>"><?php echo $url?></a>
 
-<script>setTimeout(function(){location.href = "<?=$url?>";}, 3000);</script>
+<script>setTimeout(function(){location.href = "<?php echo $url?>";}, 3000);</script>
