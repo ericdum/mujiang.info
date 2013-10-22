30eedd0796d19b92f0b5967a5521e2924723fef0
ericdum <formateric@gmail.com>
2013-10-22 14:08:28 +0800

    3秒到github

---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<meta charset="UTF-8" />
+<title>木匠手记
+
+<?php $url = "https://github.com/ericdum/mujiang.info/issues/1"?>
+<div>木匠手记正在制作中，请先访问Github Blog: <a href="<?=$url?>"><?=$url?></a>
+
+<script>setTimeout(function(){location.href = "<?=$url>?>";}, 3000)</script>
