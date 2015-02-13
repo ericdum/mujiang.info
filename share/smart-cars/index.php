c6280dc5b81c6e9d898fe5d3734fbd378990ac05
ericdum <formateric@gmail.com>
2015-02-13 11:54:20 +0800

    车身上的黑科技

---
+++ share/smart-cars/index.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+
+		<title>车身上的那些黑科技</title>
+
+		<meta name="description" content="车身上那些黑科技">
+    <meta name="author" content="欢乐木酱艾瑞克 <formateric@gmail.com>">
+
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<link rel="stylesheet" href="css/reveal.min.css">
+		<link rel="stylesheet" href="css/theme/serif.css" id="theme">
+
+		<!-- For syntax highlighting -->
+		<link rel="stylesheet" href="lib/css/zenburn.css">
+
+		<!-- If the query includes 'print-pdf', include the PDF print sheet -->
+		<script>
+			if( window.location.search.match( /print-pdf/gi ) ) {
+				var link = document.createElement( 'link' );
+				link.rel = 'stylesheet';
+				link.type = 'text/css';
+				link.href = 'css/print/pdf.css';
+				document.getElementsByTagName( 'head' )[0].appendChild( link );
+			}
+		</script>
+
+		<!--[if lt IE 9]>
+		<script src="lib/js/html5shiv.js"></script>
+		<![endif]-->
+    <?php include("../../ga.php"); ?>
+	</head>
+
+	<body>
+		<div class="reveal">
+
+			<!-- Any section element inside of this container is displayed as a slide -->
+			<div class="slides">
+				<section>
+					<h4>The Hei Ke Ji Of Che</h4>
+					<h2>现代汽车身上那些黑科技</h2>
+          <br>
+					<p>
+          <small>Created by <a target="_blank" href="http://mujiang.info">木酱</a> / <a target="_blank" href="http://weibo.com">@欢乐木匠艾瑞克</a></small>
+					</p>
+				</section>
+
+				<section>
+          <h2>ECU和他的小伙伴们</h2>
+          <p class="fragment">轮速传感器</p>
+          <p class="fragment">加速度传感器</p>
+          <p class="fragment">方向盘角度传感器</p>
+          <p class="fragment">制动压力传感器</p>
+          <p class="fragment">碰撞传感器</p>
+          <p class="fragment">雷达</p>
+          <p class="fragment">摄像头</p>
+          <p class="fragment">……</p>
+					<aside class="notes">
+            汽车上的ECU不知道是什么时候发明的。作为各种系统的控制器，他成就了下面所有的革命。
+					</aside>
+				</section>
+
+				<section>
+          <section>
+            <h2>最常见的几个安全系统</h2>
+            <p class="fragment">ABS 防抱死系统<p>
+            <p class="fragment">TCS 牵引力控制系统<p>
+            <p class="fragment">EBD 制动力分配系统<p>
+            <p class="fragment">ESP 车身稳定系统<p>
+            <p class="fragment">BAS 刹车辅助系统</p>
+            <!-- To Hit 's' -->
+            <aside class="notes">
+              最早是ABS，大概上个世纪70年代就被使用在汽车上。</br>
+            </aside>
+          </section>
+          <section>
+            <embed src="http://player.youku.com/player.php/sid/XMTc4MTcyMjAw/v.swf" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+          </section>
+				</section>
+
+				<section>
+					<h2>车内的安全系统</h2>
+					<p class="fragment">安全气囊、气帘、膨胀式安全带</p>
+					<p class="fragment">主动安全头枕、感应式座椅和头枕</p>
+					<p class="fragment">安全带预紧</p>
+				</section>
+
+				<section>
+          <h2>雷达、视频分析技术</h2>
+					<p class="fragment">车道偏离</p>
+					<p class="fragment">ACC自适应巡航</p>
+					<p class="fragment">盲区辅助</p>
+          <p class="fragment">主动防碰撞</p>
+        </section>
+
+				<section>
+          <h2>省油技术哪家强？</h2>
+
+					<p class="fragment">各种发动机技术</p>
+					<p class="fragment">制动力回收</p>
+					<p class="fragment">自动启停</p>
+				</section>
+
+				<section>
+          <h2>黑科技小菜</h2>
+          <p>陡坡缓降</p>
+          <p>紧急刹车提示</p>
+				</section>
+
+				<section>
+          <section>
+            <h2>真正的黑科技</h2>
+            <p class="fragment">矩阵LED大灯</p>
+            <p class="fragment">
+              <img src="http://img1.bitautoimg.com/bitauto/2014/01/23/a52400b5-a9c8-4f10-bdf5-e9a153bbad25_630.jpg" />
+            </p>
+          </section>
+
+          <section>
+            <h2>奥迪灯具厂</h2>
+            <p class="fragment">
+              <img src="http://img1.bitautoimg.com/bitauto/2014/01/23/185ebfff-8b17-404e-b8d5-3e69d6c9f641_630.jpg" />
+            </p>
+          </section>
+				</section>
+
+				<section>
+          <section>
+            <h2>奔驰 Magic Body Control</h2>
+            <p>
+              <img src="./imgs/mercedes-benz-chicken-magic-body-control.gif" />
+            </p>
+          </section>
+          <section>
+            <embed src="http://player.youku.com/player.php/sid/XODM0NDc0MjY0/v.swf" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+          </section>
+				</section>
+
+				<section>
+          <h2>最后, 自动驾驶</h2>
+          <embed src="http://player.youku.com/player.php/sid/XNjQyMDI4OTM2/v.swf" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+				</section>
+        <aside class="notes">
+          1分47或2分20
+        </aside>
+
+			</div>
+
+		</div>
+
+		<script src="lib/js/head.min.js"></script>
+		<script src="js/reveal.min.js"></script>
+
+		<script>
+
+			// Full list of configuration options available here:
+			// https://github.com/hakimel/reveal.js#configuration
+			Reveal.initialize({
+				controls: true,
+				progress: true,
+				history: true,
+				center: true,
+
+				theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+				transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+
+				// Parallax scrolling
+				// parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
+				// parallaxBackgroundSize: '2100px 900px',
+
+				// Optional libraries used to extend on reveal.js
+				dependencies: [
+					{ src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+					{ src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+					{ src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+					{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+					{ src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+					{ src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+				]
+			});
+
+		</script>
+
+    <div id="clock" style="position: fixed; z-index: 999999;right: 10px;top: 5px;color: #aeaeae;font-size: 20px;">00:00</div>
+    <script>
+      var start = new Date().getTime();
+      var clock = document.getElementById('clock');
+      tick = function(){
+        var duration = Math.floor((new Date().getTime() - start) / 1000 );
+        var sec = duration % 60;
+        var min = Math.floor(duration / 60);
+        if(sec < 10) sec = '0'+sec
+        if(min < 10) min = '0'+min
+        clock.innerText = min+":"+sec
+        setTimeout(tick, 1000)
+      }
+      setTimeout(tick, 1000)
+    </script>
+
+	</body>
+</html>
